994718ab5185ae985c44379f5742097e07a2670e
Simi Saha <94763668+simisaha01@users.noreply.github.com>
2024-11-21 20:06:43 -0700

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,27 +1,18 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get form data
+
     $name = htmlspecialchars(trim($_POST['name']));
     $email = htmlspecialchars(trim($_POST['email']));
     $message = htmlspecialchars(trim($_POST['message']));
-
-    // Your email address where the message should be sent
-    $to = "simisahajobs@gmail.com";  // Replace with your email
-
-    // Email subject
+    $to = "simisahajobs@gmail.com";  
     $subject = "New Contact Form Submission from: $name";
-
-    // Email content
     $body = "You have received a new message from your contact form.\n\n";
     $body .= "Name: $name\n";
     $body .= "Email: $email\n\n";
     $body .= "Message:\n$message\n";
-
-    // Email headers
     $headers = "From: $email\r\n";
     $headers .= "Reply-To: $email\r\n";
 
-    // Send the email
     if (mail($to, $subject, $body, $headers)) {
         echo "Message sent successfully!";
     } else {
